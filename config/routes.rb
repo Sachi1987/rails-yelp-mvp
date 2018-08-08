68013afd37835ee774dc0031c0ce73c5704c9756
Sachi Saito <sachi1210@me.com>
2018-08-08 17:30:20 -0300

    restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
+
+
+  resources :restaurants do
+    resources :reviews
+
+    collection do                       # collection => no restaurant id in URL
+      get 'top', to: "restaurants#top"  # RestaurantsController#top
+    end
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!(name: 'Mitsuba', address: 'Maracana', phone_number: '21-97270-1210', category: 'japanese')
+Restaurant.create!(name: 'Italy', address: 'Copa', phone_number: '21-22233-1210', category: 'italian')
+Restaurant.create!(name: 'Fran Fran', address: 'Ipanema', phone_number: '21-91340-1210', category: 'french')
+Restaurant.create!(name: 'China', address: 'Botafogo', phone_number: '21-97270-1440', category: 'chinese')
+Restaurant.create!(name: 'Bel Ble', address: 'Leblon', phone_number: '21-97270-1000', category: 'belgian')
